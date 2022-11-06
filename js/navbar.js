29192d8d56522c3de9f9e79818261ed5843cde1c
Tomin Joy <tominjk007@gmail.com>
2022-11-06 10:45:54 +0530

    chore: added hover effect to sidebar

--- js/navbar.js
+++ js/navbar.js
@@ -18,3 +18,16 @@ function navtoggle() {
     else
         ham.innerText="=";
 }
+for(const link of document.getElementsByClassName("link")) {
+    link.onmousemove = e => {
+      const decimal = e.clientX / link.offsetWidth;
+      
+      const basePercent = 80,
+            percentRange = 20,
+            adjustablePercent = percentRange * decimal;
+      
+      const lightBluePercent = basePercent + adjustablePercent;
+      
+      link.style.setProperty("--light-blue-percent", `${lightBluePercent}%`);
+    }
+  }
